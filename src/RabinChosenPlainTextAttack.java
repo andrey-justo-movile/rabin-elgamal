771a9418cceff642ce02d005ebed0ebbd39a4e9e
andrey-justo-movile <andrey.justo@movile.com>
2016-10-09 17:52:34 -0300

    Knowing chosen-plainText attack.

---
+++ src/RabinChosenPlainTextAttack.java
@@ -0,0 +1,62 @@
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class RabinChosenPlainTextAttack {
+
+    private static Set<BigInteger> cipheredMessages = new HashSet<>();
+    private static String choosenText = "Hello world!";
+    private static Long attempts = 10000L;
+    public static void main(String[] args) throws UnsupportedEncodingException {
+
+        System.out.println("Choosen Text: " + choosenText);
+
+        for (int i = 0; i < attempts; i++) {
+            List<String> decryptedText = new ArrayList<>(); // four possibilities
+
+            BigInteger[] key = Rabin.genKey(3072);
+            BigInteger N = key[0];
+            BigInteger p = key[1]; // private key
+            BigInteger q = key[2]; // private key
+
+            BigInteger message = new BigInteger(choosenText.getBytes(Charset.forName("ascii")));
+            BigInteger cipherText = Rabin.encrypt(message, N);
+
+            BigInteger[] decryptedMessage = Rabin.decrypt(cipherText, p, q);
+            for(BigInteger factors: decryptedMessage) {
+                decryptedText.add(new String(factors.toByteArray(), Charset.forName("ascii")));
+            }
+
+            cipheredMessages.add(cipherText);
+
+
+            //            System.out.println("For ->");
+            //            System.out.println("Private keys -> p: " + p);
+            //            System.out.println("Private keys -> q: " + q);
+            //            System.out.println("Public key -> N: " + N);
+            //            System.out.println("Encrypted Text: " + decryptedText);
+            //            System.out.println("Results:");
+            //            System.out.println("----------------------------");
+            //            decryptedText.forEach( d -> {
+            //                System.out.println("Decrypted Text: " + d);
+            //            });
+        }
+
+        if (cipheredMessages.size() == 1) {
+            System.out.println("Not probabilistic method. ");
+            System.out.println("Always returning same: " + cipheredMessages.toArray()[0]);
+        } else {
+            System.out.println("Probabilistic method");
+            System.out.println("Returning different ciphered messages: ");
+            cipheredMessages.forEach(cipherM -> {
+                System.out.println(cipherM);
+            });
+        }
+
+    }
+
+}
